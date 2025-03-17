8abe7a42c6dac2791e1a3573d07406bc8322d703
Tomás Delizia <68579667+tomasDelizia@users.noreply.github.com>
2025-03-17 09:39:59 +0100

    Add User interface and enhance Product interface with additional fields and response structure

--- src/app/products/interfaces/product.interfaces.ts
+++ src/app/products/interfaces/product.interfaces.ts
@@ -1,7 +1,37 @@
+import { User } from "@auth/interfaces/auth.interfaces";
+
+export interface ProductsResponse {
+  count:    number;
+  pages:    number;
+  products: Product[];
+}
+
 export interface Product {
-  id: number;
-  name: string;
+  id:          string;
+  title:       string;
+  price:       number;
   description: string;
-  price: number;
-  imageUrl: string;
+  slug:        string;
+  stock:       number;
+  sizes:       Size[];
+  gender:      Gender;
+  tags:        string[];
+  images:      string[];
+  user:        User;
+}
+
+export enum Gender {
+  Men = "men",
+  Unisex = "unisex",
+  Women = "women",
+  Kid = "kid",
+}
+
+export enum Size {
+  L = "L",
+  M = "M",
+  S = "S",
+  Xl = "XL",
+  Xs = "XS",
+  Xxl = "XXL",
 }
