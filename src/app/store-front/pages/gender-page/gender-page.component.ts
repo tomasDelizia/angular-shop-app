c20638491ce552f5aee61dc4abc67516dce275ec
tdeliziae <tdeliziae@eservicios.indra.es>
2025-05-08 13:19:57 +0200

    Implement gender-based product retrieval in GenderPageComponent

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductListComponent } from '../../../products/components/product-list/product-list.component';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductListComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+
+  gender = toSignal(
+    this.activatedRoute.params.pipe(map(({ gender }) => gender))
+  );
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({ gender: request.gender });
+    },
+  });
+}
