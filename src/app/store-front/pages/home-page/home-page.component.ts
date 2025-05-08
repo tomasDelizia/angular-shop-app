5dd229a813e55b94e5f406cc78f2a7f5cd747236
tdeliziae <tdeliziae@eservicios.indra.es>
2025-05-08 16:45:34 +0200

    Refactor HomePageComponent to integrate pagination service for current page state management

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductsService } from '@products/services/products.service';
 import { ProductListComponent } from '@products/components/product-list/product-list.component';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -12,10 +13,14 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
 export class HomePageComponent {
   productsService = inject(ProductsService);
 
+  paginationService = inject(PaginationService);
+
   productsResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+      });
     },
   });
 }
