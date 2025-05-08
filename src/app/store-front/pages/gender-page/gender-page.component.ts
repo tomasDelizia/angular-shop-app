acc71b79474027ce6e6a4b8441de7a9150e0bf27
tdeliziae <tdeliziae@eservicios.indra.es>
2025-05-08 16:49:14 +0200

    Refactor GenderPageComponent to integrate pagination functionality for product retrieval

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -4,14 +4,18 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
 import { map } from 'rxjs';
 import { ProductListComponent } from '../../../products/components/product-list/product-list.component';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
   activatedRoute = inject(ActivatedRoute);
+
+  paginationService = inject(PaginationService);
 
   gender = toSignal(
     this.activatedRoute.params.pipe(map(({ gender }) => gender))
@@ -20,9 +24,15 @@ export class GenderPageComponent {
   productsService = inject(ProductsService);
 
   productsResource = rxResource({
-    request: () => ({ gender: this.gender() }),
+    request: () => ({
+      gender: this.gender(),
+      page: this.paginationService.currentPage() - 1,
+    }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({ gender: request.gender });
+      return this.productsService.getProducts({
+        gender: request.gender,
+        offset: request.page * 9,
+      });
     },
   });
 }
