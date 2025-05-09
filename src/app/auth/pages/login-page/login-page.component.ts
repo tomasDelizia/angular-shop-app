0fefc340b91917e173e242f705aec8687797fe6d
tdeliziae <tdeliziae@eservicios.indra.es>
2025-05-09 10:41:46 +0200

    Implement reactive form for login page with validation and error handling

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+    const { email = '', password = '' } = this.loginForm.value;
+    console.log('Form submitted:', { email, password });
+  }
+}
