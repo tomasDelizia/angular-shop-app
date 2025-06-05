1f18be1a23faec41667c762c6920d7d7000b297a
tdeliziae <tdeliziae@eservicios.indra.es>
2025-06-05 17:08:12 +0200

    Add product details component and update admin page to display product information

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { ProductsService } from '@products/services/products.service';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @Component({
   selector: 'app-product-admin-page',
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './product-admin-page.component.html',
 })
 export class ProductAdminPageComponent {
