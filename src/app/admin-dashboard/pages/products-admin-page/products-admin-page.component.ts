fd6937e8df73ece57c75d84a9a4d5835a13fa3cf
tdeliziae <tdeliziae@eservicios.indra.es>
2025-06-05 16:18:39 +0200

    Add pagination component and update product table binding in admin page

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductTableComponent } from '../../../products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTableComponent],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent {}
+export class ProductsAdminPageComponent {
+  productsService = inject(ProductsService);
+
+  paginationService = inject(PaginationService);
+
+  productsResource = rxResource({
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+      });
+    },
+  });
+}
