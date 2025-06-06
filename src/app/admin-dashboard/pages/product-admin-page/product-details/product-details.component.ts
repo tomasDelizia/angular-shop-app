5caa27af75e4f07068f990163b7c4976c14ee37f
tdeliziae <tdeliziae@eservicios.indra.es>
2025-06-06 10:39:22 +0200

    Implement reactive form for product details with validation

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,14 +1,51 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Product } from '@products/interfaces/product.interfaces';
 import { ProductCarouselComponent } from '../../../../products/components/product-carousel/product-carousel.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent],
+  imports: [ProductCarouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product = input.required<Product>();
 
+  fb = inject(FormBuilder);
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    slug: [
+      '',
+      [Validators.required, Validators.pattern(FormUtils.slugPattern)],
+    ],
+    description: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [['']],
+    tags: [''],
+    gender: [
+      'men',
+      [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
+    ],
+  });
+
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  // Partial<Product> is used to allow partial updates to the form
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
+    // this.productForm.patchValue(formLike as any);
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
