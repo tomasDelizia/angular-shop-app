3072564adb6bc6713fc22679aaa5d0c0bc8a5292
tdeliziae <tdeliziae@eservicios.indra.es>
2025-06-09 16:37:06 +0200

    Implement OnChanges lifecycle hook in ProductCarouselComponent to handle image updates

--- src/app/products/components/product-carousel/product-carousel.component.ts
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -3,6 +3,8 @@ import {
   Component,
   ElementRef,
   input,
+  OnChanges,
+  SimpleChanges,
   viewChild,
 } from '@angular/core';
 import Swiper from 'swiper';
@@ -24,16 +26,33 @@ import { ProductImagePipe } from '@products/pipes/product-image.pipe';
   }
   `,
 })
-export class ProductCarouselComponent implements AfterViewInit {
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
   productName = input.required<string>();
   images = input.required<string[]>();
   swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+  swiper: Swiper | undefined = undefined;
 
   ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // Check if the images input has changed and is not the first change
+    if (changes['images'].firstChange) return;
+    if (!this.swiper) return;
+    // If swiper is already initialized, destroy it and reinitialize
+    this.swiper.destroy(true, true);
+    const paginationElement: HTMLDivElement =
+      this.swiperDiv().nativeElement?.querySelector('.swiper-pagination');
+    paginationElement.innerHTML = ''; // Clear pagination element
+    setTimeout(() => this.swiperInit(), 100); // Wait for the DOM to update
+  }
+
+  swiperInit(): void {
     const element = this.swiperDiv().nativeElement;
     if (!element) return;
 
-    const swiper = new Swiper(element, {
+    this.swiper = new Swiper(element, {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
